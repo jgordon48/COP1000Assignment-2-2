2399fe778c102ba654e1c51ef30436857673e77b
jgordon48 <159393742+jgordon48@users.noreply.github.com>
2024-02-26 02:23:18 +0000

    Assignment 2-2

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # Output: Your current age followed by your age in 2050
 
 # Create your variables here
-
+myCurrentAge = 25
+currentYear = 2024
 myNewAge = myCurrentAge + (2050 - currentYear)
 print("My Current Age is " + str(myCurrentAge))
 print("I will be " + str(myNewAge) + " in 2050.")
